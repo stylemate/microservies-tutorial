9b957e751acd1260881c1cd3236de634494d6a9c
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2023-04-28 11:15:01 +0900

    convert to monorepo structure, nest g app reservations

--- apps/sleeper/src/app.module.ts
+++ apps/sleeper/src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule, ConfigModule } from '@app/common';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { Form } from '../entities/Form';
 
 @Module({
   imports: [
